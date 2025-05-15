226a343958c8118e268925ed31c1952af9bc62b0
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-15 14:23:01 +1200

    gf

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
 import sqlite3
 
-db = sqlite3.connect("pokemon.db")
-surcor = db.cursor()
-sql = "select * from pokemon"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close()
+#variable
+DATABASE = "pokemon.db"
+
+
+#function
+def print_all_pokemon():
+    db = sqlite3.connect("pokemon.db")
+    cursor = db.cursor()
+    sql = "select * from pokemon"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for pokemon in results:
+        print(f"pokedex: {pokemon[0]}, pokemon: {pokemon[1]}, type: {pokemon[2]}")
+    db.close()
+
+
+
+#maincode
+print_all_pokemon()
