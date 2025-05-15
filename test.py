f59abf75387b22ec8d49a25ebe828a23c0ca0aed
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-15 14:05:46 +1200

    gb

--- test.py
+++ test.py
@@ -1,16 +1,9 @@
 import sqlite3
 
-DATABASE = 'pokemon.db'
-
-def print_all_pokemon():
-    with sqlite3.connect(DATABASE) as db:
-        cursor = db.cursor()
-        sql = "SELECT * FROM pokemon;"
-        cursor.execute(sql)
-        results = cursor.fetchall()
-
-        for pokemon in results:
-            print(f'pokedex: {pokemon[0]} name: {pokemon[1]} type: {pokemon[2]}')
-
-if __name__ == "__main__":
-    print_all_pokemon()
+db = sqlite3.connect("pokemon.db")
+surcor = db.cursor()
+sql = "select * from pokemon"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+db.close()
