36b1114c0408b929a13c07f9328316cd51843b58
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-20 11:53:01 +1200

    g

--- test.py
+++ test.py
@@ -11,11 +11,42 @@ def print_all_pokemon():
     sql = "select * from pokemon"
     cursor.execute(sql)
     results = cursor.fetchall()
+    print('dex number     pokemon name          type')
     for pokemon in results:
-        print(f"pokedex: {pokemon[0]}, pokemon: {pokemon[1]}, type: {pokemon[2]}")
+        print(f"pokedex: {pokemon[0]:<5} pokemon: {pokemon[1]:<12} type: {pokemon[2]:<10}")
     db.close()
 
+def print_all_water():
+    db = sqlite3.connect("pokemon.db")
+    cursor = db.cursor()
+    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Water%';"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print('dex number     pokemon name          type')
+    for pokemon in results:
+        print(f"pokedex: {pokemon[0]:<5} pokemon: {pokemon[1]:<12} type: {pokemon[2]:<10}")
+    db.close()
 
+def print_only_water():
+    db = sqlite3.connect("pokemon.db")
+    cursor = db.cursor()
+    sql = "SELECT * FROM pokemon WHERE pokemon_type = 'Water';"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print('dex number     pokemon name          type')
+    for pokemon in results:
+        print(f"pokedex: {pokemon[0]:<5} pokemon: {pokemon[1]:<12} type: {pokemon[2]:<10}")
+    db.close()
 
+#What would you like to do.\n1. Print all pokemon\n2. Print all water type pokemons            3. Print water ONLY pokemons\n
 #maincode
-print_all_pokemon()
+while True:
+    user_input = input('To print all pokemons, write "all"\nTo print pokemons that are the same types, write "Print...."\nExample: "Print water" or "Print fire"\n')
+    if user_input == "all":
+        print_all_pokemon()
+    elif user_input =="Print water":
+        print_all_water()
+    elif user_input == "Print only water":
+        print_only_water()
+    elif user_input == "break":
+        break
