4ac4cb472e706f2ac8abd4ea17b56925edec2241
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-21 10:06:02 +1200

    kallum

--- test.py
+++ test.py
@@ -4,212 +4,29 @@ import sqlite3
 DATABASE = "pokemon.db"
 
 #function
-def print_all_bug():
+def print_all_type(poke_type_string):
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Bug%';"
+    sql = f"SELECT * FROM pokemon WHERE lower(pokemon_type) LIKE '%{poke_type_string}%';"
     cursor.execute(sql)
     results = cursor.fetchall()
     print('dex number  pokemon name    type')
     for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
+        print(f"{pokemon[0]:<11} {pokemon[1]:<15} {pokemon[2]:<10}")
     db.close()
 
-def print_all_dark():
+def print_only_type(poke_type_string):
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Dark%';"
+    sql = f"SELECT * FROM pokemon WHERE lower(pokemon_type) == '{poke_type_string}';"
     cursor.execute(sql)
     results = cursor.fetchall()
     print('dex number  pokemon name    type')
     for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
+        print(f"{pokemon[0]:<11} {pokemon[1]:<15} {pokemon[2]:<10}")
     db.close()
 
-def print_all_dragon():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Dragon%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
 
-def print_all_electric():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Electric%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-
-def print_all_fairy():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Fairy%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-
-def print_all_fighting():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Fighting%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-
-def print_all_fire():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Fire%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-
-def print_all_flying():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Flying%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-
-def print_all_ghost():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Ghost%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-
-def print_all_grass():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Grass%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-
-def print_all_ground():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Ground%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-
-def print_all_ice():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Ice%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-
-def print_all_normal():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Normal%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-
-def print_all_poison():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Poison%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-
-def print_all_psychic():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Psychic%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-
-def print_all_rock():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Rock%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-
-def print_all_steel():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Steel%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-
-def print_all_water():
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Water%';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM pokemon WHERE pokemon_type = 'Flying';"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print('dex number  pokemon name    type')
-    for pokemon in results:
-        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
-    db.close()
 #What would you like to do.\n1. Print all pokemon\n2. Print all water type pokemons            3. Print water ONLY pokemons\n
 #maincode
 while True:
@@ -219,79 +36,17 @@ while True:
     user_input = input('If you want to finish type "finish"\n')
     if user_input == "all":
         print_all_pokemon()
-    elif "print" in user_input:
-        if "bug" in user_input:
-            print_all_bug()
-        elif "dark" in user_input:
-            print_all_dark()
-        elif "dragon" in user_input:
-            print_all_dragon()
-        elif "electric" in user_input:
-            print_all_electric()
-        elif "fairy" in user_input:
-            print_all_fairy()
-        elif "fighting" in user_input:
-            print_all_fighting()
-        elif "fire" in user_input:
-            print_all_fire()
-        elif "flying" in user_input:
-            print_all_flying()
-        elif "ghost" in user_input:
-            print_all_ghost()
-        elif "grass" in user_input:
-            print_all_grass()
-        elif "ground" in user_input:
-            print_all_ground()
-        elif "ice" in user_input:
-            print_all_ice()
-        elif "normal" in user_input:
-            print_all_normal()
-        elif "poison" in user_input:
-            print_all_poison()
-        elif "psychic" in user_input:
-            print_all_psychic()
-        elif "rock" in user_input:
-            print_all_rock()
-        elif "steel" in user_input:
-            print_all_steel()
-        elif "water" in user_input:
-            print_all_water()
-    elif "only" in user_input:
-        if "bug" in user_input:
-            print_all_bug()
-        elif "dark" in user_input:
-            print_all_dark()
-        elif "dragon" in user_input:
-            print_all_dragon()
-        elif "electric" in user_input:
-            print_all_electric()
-        elif "fairy" in user_input:
-            print_all_fairy()
-        elif "fighting" in user_input:
-            print_all_fighting()
-        elif "fire" in user_input:
-            print_all_fire()
-        elif "flying" in user_input:
-            print_all_flying()
-        elif "ghost" in user_input:
-            print_all_ghost()
-        elif "grass" in user_input:
-            print_all_grass()
-        elif "ground" in user_input:
-            print_all_ground()
-        elif "ice" in user_input:
-            print_all_ice()
-        elif "normal" in user_input:
-            print_all_normal()
-        elif "poison" in user_input:
-            print_all_poison()
-        elif "psychic" in user_input:
-            print_all_psychic()
-        elif "rock" in user_input:
-            print_all_rock()
-        elif "steel" in user_input:
-            print_all_steel()
-        elif "water" in user_input:
-            print_all_water()
+
+    elif user_input.startswith("print "):
+        words = user_input.split()
+        if len(words) >= 2:
+            poke_type_string = words[1]
+            print_all_type(poke_type_string)
+
+    elif user_input.startswith("only "):
+        words = user_input.split()
+        if len(words) >= 2:
+            poke_type_string = words[1]
+            print_only_type(poke_type_string)   
     elif user_input == "finish":
         break
