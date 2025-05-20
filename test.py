c04373f054a0c9678b4b94fed16666f03fc5317d
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-20 12:05:06 +1200

    mk

--- test.py
+++ test.py
@@ -11,9 +11,9 @@ def print_all_pokemon():
     sql = "select * from pokemon"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print('dex number     pokemon name          type')
+    print('dex number  pokemon name    type')
     for pokemon in results:
-        print(f"pokedex: {pokemon[0]:<5} pokemon: {pokemon[1]:<12} type: {pokemon[2]:<10}")
+        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
     db.close()
 
 def print_all_water():
@@ -22,9 +22,9 @@ def print_all_water():
     sql = "SELECT * FROM pokemon WHERE pokemon_type LIKE '%Water%';"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print('dex number     pokemon name          type')
+    print('dex number  pokemon name    type')
     for pokemon in results:
-        print(f"pokedex: {pokemon[0]:<5} pokemon: {pokemon[1]:<12} type: {pokemon[2]:<10}")
+        print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
     db.close()
 
 def print_only_water():
@@ -33,7 +33,7 @@ def print_only_water():
     sql = "SELECT * FROM pokemon WHERE pokemon_type = 'Water';"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print('dex number  pokemon name     type')
+    print('dex number  pokemon name    type')
     for pokemon in results:
         print(f"{pokemon[0]:<5} {pokemon[1]:<12} {pokemon[2]:<10}")
     db.close()
@@ -41,12 +41,15 @@ def print_only_water():
 #What would you like to do.\n1. Print all pokemon\n2. Print all water type pokemons            3. Print water ONLY pokemons\n
 #maincode
 while True:
-    user_input = input('To print all pokemons, write "all"\nTo print pokemons that are the same types, write "Print...."\nExample: "Print water" or "Print fire"\n')
+    print('To print all pokemons, write "all"\n')
+    print('To print pokemons that are the same types, write "Print...."\nExample: "Print water" or "Print fire"\n')
+    print('If you want to print pokemons that are only one specific type, enter "Print only....."\nExample: "Print only water" or "Print only fire"\n')
+    user_input = input('If you want to finish type "finish"')
     if user_input == "all":
         print_all_pokemon()
-    elif user_input =="Print water":
+    elif user_input == "Print water":
         print_all_water()
     elif user_input == "Print only water":
         print_only_water()
-    elif user_input == "break":
+    elif user_input == "finish":
         break
