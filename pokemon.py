1c13ae0e1a5defb0876fadbe58c38295b670ccb8
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-27 08:51:56 +1200

    added comments

--- pokemon.py
+++ pokemon.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-#variable
+#variables
 DATABASE = "pokemon.db"
 VALID_TYPES = [
     "normal", "fire", "water", "grass", "electric", "ice", "fighting", "poison",
@@ -8,28 +8,31 @@ VALID_TYPES = [
     "steel", "fairy"
 ]
 #functions
+#functino for printing all pokemons
 def print_all_pokemon():
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     sql = f"SELECT * FROM pokemon;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print('dex number  pokemon name    type')
+    print('dex number | pokemon name  |  type')
     for pokemon in results:
-        print(f"{pokemon[0]:<11} {pokemon[1]:<15} {pokemon[2]:<10}")
+        print(f"{pokemon[0]:<12} {pokemon[1]:<16} {pokemon[2]:<11}")
     db.close()
-    
+
+#function for printing all pokemons of a type
 def print_all_type(poke_type_string):
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     sql = f"SELECT * FROM pokemon WHERE lower(pokemon_type) LIKE '%{poke_type_string}%';"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print('dex number  pokemon name    type')
+    print('dex number | pokemon name  |  type')
     for pokemon in results:
-        print(f"{pokemon[0]:<11} {pokemon[1]:<15} {pokemon[2]:<10}")
+        print(f"{pokemon[0]:<12} {pokemon[1]:<16} {pokemon[2]:<11}")
     db.close()
 
+#function for printing all pokemons of only one type
 def print_only_type(poke_type_string):
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
@@ -42,15 +45,18 @@ def print_only_type(poke_type_string):
     db.close()
 
 
-#What would you like to do.\n1. Print all pokemon\n2. Print all water type pokemons            3. Print water ONLY pokemons\n#maincode
+#ask what the user wants to print
 while True:
     print('To print all pokemons, write "all"\n')
     print('To print pokemons that are the same types, write "print...."\nExample: "rint water" or "print fire"\n')
     print('If you want to print pokemons that are only one specific type, enter "only....."\nExample: "only water" or "only fire"\n')
     user_input = input('If you want to finish type "finish"\n')
+
+#code for when someone wants to print all pokemons
     if user_input == "all":
         print_all_pokemon()
 
+# code for when someone wants to print pokemons of a type
     elif user_input.startswith("print "):
         words = user_input.split()
         if len(words) >= 2:
@@ -60,6 +66,7 @@ while True:
             else:
                 print("That is not a type.")
 
+#code for when the user wants to print pokemons of only one type
     elif user_input.startswith("only "):
         words = user_input.split()
         if len(words) >= 2:
@@ -71,5 +78,6 @@ while True:
     elif user_input == "finish":
         break
     
+    #Solution for non pokemon type inputs
     else:
         print("Invalid input. Try again.")
