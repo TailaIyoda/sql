2f0018837a8787d8a65101780e3100385a512149
TailaIyoda <162512077+TailaIyoda@users.noreply.github.com>
2025-05-30 18:21:27 +1200

    added title and decoration

--- pokemon.py
+++ pokemon.py
@@ -69,7 +69,13 @@ def search_pokemon_dex():
 
 #ask what the user wants to print
 
-
+print('======== \033[38;5;208mPokemon Generation 1 Database \033[0m========')
+print('\033[1m     _____           _____           _____')
+print('\033[1m   =       =       =       =       =       =')
+print('\033[1m =    ___    =   =    ___    =   =    ___    =')
+print('\033[1m ====     ====   ====     ====   ====     ====')
+print('\033[1m =    ---    =   =    ---    =   =    ---    =')
+print('\033[1m   = _____ =       = _____ =       = _____ =')
 while True:  #keeps asking until input is 6 (exit)
     first_input = input('\n\033[1;36mWhich pokemons do you want to print?\n\033[38;5;208m1.\033[0m Print all pokemons\n\033[38;5;208m2.\033[0m select pokemons by pokemon type \n\033[38;5;208m3.\033[0m select pokemons with only one type\n\033[38;5;208m4.\033[0m search by name \n\033[38;5;208m5.\033[0m search by pokedex number\n\033[38;5;208m6.\033[0m exit\n')
 
